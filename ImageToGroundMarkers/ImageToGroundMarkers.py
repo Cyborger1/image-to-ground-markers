f8b7008a278540cb3e4fc497cb66238db498685b
Cyborger1 <45152844+Cyborger1@users.noreply.github.com>
2021-01-27 02:20:16 -0500

    Make code even more better

--- ImageToGroundMarkers/ImageToGroundMarkers.py
+++ ImageToGroundMarkers/ImageToGroundMarkers.py
@@ -12,7 +12,7 @@ def wp_to_region(wpX, wpY, plane):
 def rgba_to_int(r, g, b, a=0xff):
     alpha = a & 0xff
     val = (alpha << 24) | ((r & 0xff) << 16) | ((g & 0xff) << 8) | (b & 0xff)
-    return ctypes.c_long(val).value, alpha == 0
+    return ctypes.c_long(val).value, alpha
 
 def image_to_ground_markers(imagePath, coordsTuple):
     if not(len(coordsTuple) == 3 or len(coordsTuple) == 4):
@@ -24,33 +24,38 @@ def image_to_ground_markers(imagePath, coordsTuple):
         startWP = coordsTuple
 
     imageFile = Image.open(imagePath, 'r')
-    imageSize = imageFile.size
+
+    colorFunctions = {
+        'RGB': rgba_to_int,
+        'RGBA': rgba_to_int
+    }
+
+    imageMode = imageFile.mode
+    if imageMode not in colorFunctions:
+        raise IOError('Image format {} is not supported.'.format(imageMode))
+
+    colorFunc = colorFunctions[imageMode]
+    imageX, imageY = imageFile.size
     imageArray = imageFile.load()
 
     exportValues = []
 
-    for x in range(imageSize[0]):
-        for y in range (imageSize[1]):
-            wpX = startWP[0] + x
-            wpY = startWP[1] + y
-            plane = startWP[2]
-            pixel = imageArray[x, imageSize[1] - y - 1]
-            if len(pixel) == 3 or len(pixel) == 4:
-                color, transparent = rgba_to_int(*pixel)
-        
-                if not(transparent):
-                    regionInfo = wp_to_region(wpX, wpY, plane)
+    for x in range(imageX):
+        for y in range (imageY):
+            color, alpha = colorFunc(*imageArray[x, imageY - y - 1])
+            if alpha > 0:
+                rId, rX, rY, plane = wp_to_region(startWP[0] + x, startWP[1] + y, startWP[2])
 
-                    valueDict = {}
-                    valueDict['regionId'] = regionInfo[0]
-                    valueDict['regionX'] = regionInfo[1]
-                    valueDict['regionY'] = regionInfo[2]
-                    valueDict['z'] = regionInfo[3]
-                    valueDict['color'] = colorDict = {}
-                    colorDict['value'] = color
-                    colorDict['falpha'] = 0.0
+                valueDict = {}
+                valueDict['regionId'] = rId
+                valueDict['regionX'] = rX
+                valueDict['regionY'] = rY
+                valueDict['z'] = plane
+                valueDict['color'] = colorDict = {}
+                colorDict['value'] = color
+                colorDict['falpha'] = 0.0
 
-                    exportValues.append(valueDict)
+                exportValues.append(valueDict)
 
     return json.dumps(exportValues)
 
