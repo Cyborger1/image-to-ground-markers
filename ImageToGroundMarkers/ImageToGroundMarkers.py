a8a0ad3aac179b6e308f8fdc3feaef680f2f9290
Cyborger1 <45152844+Cyborger1@users.noreply.github.com>
2021-01-28 11:51:07 -0500

    Add minAlpha argument

--- ImageToGroundMarkers/ImageToGroundMarkers.py
+++ ImageToGroundMarkers/ImageToGroundMarkers.py
@@ -14,7 +14,7 @@ def rgba_to_int(r, g, b, a=0xff):
     val = (alpha << 24) | ((r & 0xff) << 16) | ((g & 0xff) << 8) | (b & 0xff)
     return ctypes.c_long(val).value, alpha
 
-def image_to_ground_markers(imagePath, coordsTuple):
+def image_to_ground_markers(imagePath, coordsTuple, minAlpha=1):
     if not(len(coordsTuple) == 3 or len(coordsTuple) == 4):
         raise ValueError('coordsTuple has to be format (wpX, wpY, plane) or (regionId, rX, rY, plane).')
 
@@ -40,7 +40,7 @@ def image_to_ground_markers(imagePath, coordsTuple):
     for x in range(imageX):
         for y in range (imageY):
             color, alpha = colorFunc(*imageArray[x, imageY - y - 1])
-            if alpha > 0:
+            if alpha >= minAlpha:
                 rId, rX, rY, plane = wp_to_region(startX + x, startY + y, startPlane)
 
                 valueDict = {}
@@ -60,4 +60,4 @@ START_WP = (2257, 5332, 0)
 START_COORDS = (9043, 17, 20, 0)
 IMAGE_PATH = r'rl-30x25.png'
 
-pyperclip.copy(json.dumps(image_to_ground_markers(IMAGE_PATH, START_WP)))
+pyperclip.copy(json.dumps(image_to_ground_markers(IMAGE_PATH, START_WP, 1)))
